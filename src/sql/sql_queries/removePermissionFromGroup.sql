82e65a1bcfe37b26e97a45af57b0bea353769c93
f1sssss <filip.stankovic99@edu.ucg.ac.me>
2024-02-25 14:25:21 +0100

    Bugfix: removing permission from group

--- src/sql/sql_queries/removePermissionFromGroup.sql
+++ src/sql/sql_queries/removePermissionFromGroup.sql
@@ -1,6 +1,18 @@
-delete gr_pairing_permission_groups_permission
-where id_permission = @id_permission and id_permission_group = @id_permission_group
+delete gr_permission_properties
+where exists (
+select * from gr_pairing_permisson_property_list pl
+left join gr_permission_properties pp on pp.permission_property_id=pl.id
+left join gr_pairing_permission_groups_permission pg on pg.id_permission=pl.id_permission
+where id_permission_group=@id_permission_group and pl.id_permission=@id_permission and pp.id=gr_permission_properties.id
+)
 
-select id_permission, id_permission_group
-from gr_pairing_permission_groups_permission (nolock)
-where id_permission = @id_permission and id_permission_group = @id_permission_group
+
+delete from gr_pairing_permission_groups_permission
+where  id_permission_group=@id_permission_group and id_permission=@id_permission
+
+
+select * from gr_pairing_permisson_property_list pl
+left join gr_permission_properties pp on pp.permission_property_id=pl.id
+left join gr_pairing_permission_groups_permission pg on pg.id_permission=pl.id_permission
+where id_permission_group=@id_permission_group and pl.id_permission=@id_permission
+
