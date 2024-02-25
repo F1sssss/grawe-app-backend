131000fb87cf8a80cd9bd1faeedac78c6ae908c9
f1sssss <filip.stankovic99@edu.ucg.ac.me>
2024-02-25 15:15:48 +0100

    BUGFIX: small change to sql

--- src/sql/sql_queries/getPermissionsByGroup.sql
+++ src/sql/sql_queries/getPermissionsByGroup.sql
@@ -1,3 +1,3 @@
-select * from gr_pairing_users_groups_permission  up
+select up.permission_group_id,up.user_id,p.* from gr_pairing_users_groups_permission  up
 left join gr_permission p on p.id=up.permission_group_id
 where up.permission_group_id=@id
