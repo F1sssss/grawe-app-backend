ea36668f697af77bf20f55ed0f43e13cc52e0098
f1sssss <filip.stankovic99@edu.ucg.ac.me>
2024-03-04 10:59:24 +0100

    FIX: typo

--- src/sql/sql_queries/getPermission.sql
+++ src/sql/sql_queries/getPermission.sql
@@ -1,3 +1,32 @@
+if (@id_permission_group is null or @id_permission_group=0)
+
+begin
+
+select distinct 
+p.id permission_id,
+p.route,
+p.method,
+p.name,
+p.description,
+0 read_right,
+0 write_right,
+pl.property_path,
+pl.id
+
+from gr_permission p
+left join gr_pairing_permisson_property_list ppl on ppl.id_permission=p.id
+left join gr_property_lists pl on pl.id=ppl.id_permission_property
+left join gr_permission_properties pp on pp.permission_property_id=ppl.id
+where p.id=@id_permission --and group_id=@id_permission_group
+order by p.id
+
+
+end
+
+else
+
+begin
+
 select
 p.id permission_id,
 p.route,
@@ -13,5 +42,9 @@ from gr_permission p
 left join gr_pairing_permisson_property_list ppl on ppl.id_permission=p.id
 left join gr_property_lists pl on pl.id=ppl.id_permission_property
 left join gr_permission_properties pp on pp.permission_property_id=ppl.id
-where p.id=@id_permission and (group_id=@id_permission_group or @id_permission_group is null or @id_permission_group=0)
+where p.id=@id_permission and group_id=@id_permission_group
 order by p.id
+
+
+end
+
