57aad2b2d5485828c74a428c15a5380e5b3c5d0a
f1sssss <filip.stankovic99@edu.ucg.ac.me>
2024-03-10 16:02:38 +0100

    FEAT: Added Access control for reports

--- src/sql/sql_queries/deleteReport.sql
+++ src/sql/sql_queries/deleteReport.sql
@@ -5,9 +5,17 @@ end
 
 begin transaction
 
+declare @old_report_name nvarchar(255)=(select report_name from reports where id=@id)
+
+
 delete from reports where id = @id
 
 delete from reports_param_options where report_id = @id
+
+delete from gr_pairing_permisson_property_list where id_permission_property in (select id from gr_property_lists where property_path=@old_report_name)
+
+delete from gr_property_lists where property_path=@old_report_name
+
 
 commit
 
--- src/sql/sql_queries/insertReport.sql
+++ src/sql/sql_queries/insertReport.sql
@@ -1,6 +1,6 @@
 if exists (select * from reports where report_name=@new_report_name)
 begin
-    throw 50000, 'report-already-exists',2
+    throw 50000, 'report-name-already-exists',2
 end
 
 if not exists (select * from sys.procedures where object_id=@procedure_id)
@@ -8,6 +8,16 @@ begin
     throw 50000, 'procedure-not-found',2
 end
 
+
 insert into reports (report_name,procedure_id) values (@new_report_name,@procedure_id)
+
+insert into gr_property_lists
+select report_name from reports where report_name=@new_report_name
+
+insert into gr_pairing_permisson_property_list
+select (select distinct id from gr_permission where route='/api/v1/reports/' and method='get'), id from gr_property_lists
+where property_path=@new_report_name
+
+
 
 select * from reports where report_name=@new_report_name
